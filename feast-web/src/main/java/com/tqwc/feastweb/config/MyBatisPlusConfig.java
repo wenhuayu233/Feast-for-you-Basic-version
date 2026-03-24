8443e127e958ae7b7cf4791a79c601d3790526d2
Tang <494629876@qq.com>
2026-03-24 14:17:00 +0800

    pom文件修改 yml文件连接数据库有问题

---
+++ feast-web/src/main/java/com/tqwc/feastweb/config/MyBatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.tqwc.feastweb.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author Tang
+ * @data 2026/3/19 22:54
+ */
+@Configuration
+public class MyBatisPlusConfig {
+    //分页
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
